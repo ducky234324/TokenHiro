6cca0f73e18d9530a2bbd686783f631f317fd2c7
NeelClaudel <neel.claudel@gmail.com>
2023-11-02 19:02:18 +0100

    add:services-archive

--- components/Archive.tsx
+++ components/Archive.tsx
@@ -9,7 +9,7 @@ const Archive = () => {
     <div className="max-w-contentContainer mx-auto px-4 py-24">
       <div className="w-full flex flex-col items-center">
         <h2 className="text-3xl font-titleFont font-semibold">
-          Other Services we offer
+          Other Popular Services we offer
         </h2>
         <p className="text-sm font-titleFont text-textGreen">
           All Smart Contracts We Provide Are Audit Ready
@@ -17,14 +17,14 @@ const Archive = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-10 lgl:px-10">
         <ArchiveCard
-          title="NFT Collection"
-          des="We will study your project and create a unique NFT collection for you. We can also create a website for your NFT collection."
+          title="NFT Drop Collection"
+          des="Release Collection of unique NFTs for a set price. We will study your project and create a unique NFT collection for you. We can also create a website for your NFT collection."
           listItem={["NFT", "Metadata", "IPFS", "Blockchain", "Any Blockchain"]}
           link="https://www.fiverr.com/"
         />
         <ArchiveCard
           title="Marketplace for NFTs"
-          des=" We will create a marketplace for your NFTs. People will be able to buy and sell NFTs on your own marketplace."
+          des=" We will create a marketplace for your NFTs. People will be able to buy and sell NFTs on your own marketplace like OpenSea or Rarible."
           listItem={[
             "NFT",
             "Markeplace",
@@ -68,9 +68,8 @@ const Archive = () => {
               transition={{ delay: 0.1 }}
             >
               <ArchiveCard
-                title="OREBI e-commerce Shopping store"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="DAO and voting smart contracts"
+                des=" Setting up a governance system for your project. We will create a DAO for your project and help you to create a voting system for your project. We will also help you to create a website for your DAO."
                 listItem={["Reactjs", "Tailwind CSS", "getform.io"]}
                 link="https://www.fiverr.com/"
               />
@@ -81,9 +80,8 @@ const Archive = () => {
               transition={{ delay: 0.2 }}
             >
               <ArchiveCard
-                title="Noor Online Shopping Store"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="Edition Drop Collection"
+                des=" ERC1155 token for a set price. This option is for you if you want to create a collection of NFTs with different editions. You will be able to sell One artwork multiple times. For project like game, DeFi, token access, etc."
                 listItem={["Reactjs", "Tailwind CSS", "redux-toolkit"]}
                 link="https://www.fiverr.com/"
               />
@@ -94,9 +92,8 @@ const Archive = () => {
               transition={{ delay: 0.3 }}
             >
               <ArchiveCard
-                title="Personalized Blog"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="Loyalty Card NFT"
+                des=" Loyalty Program for your project but it use NFT as card for your community, allow project onwer to reward their community with NFT card and airdrop easily. We will create a website for your NFT collection."
                 listItem={["Nextjs", "Tailwind CSS", "next-auth", "firebase"]}
                 link="https://www.fiverr.com/"
               />
@@ -107,9 +104,8 @@ const Archive = () => {
               transition={{ delay: 0.4 }}
             >
               <ArchiveCard
-                title="Shoppers Online Shopping Market"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="Stack ERC20 Token"
+                des=" Deploy a token and stacking contract for your project. We will create a token for you and deploy a stacking contract for your token. We will also create a website for your stacking contract where user can deposit tokens and earn reward."
                 listItem={["Nextjs", "react-slick", "redux-toolkit"]}
                 link="https://www.fiverr.com/"
               />
@@ -120,9 +116,8 @@ const Archive = () => {
               transition={{ delay: 0.5 }}
             >
               <ArchiveCard
-                title="Single Page Personalized Portfolio"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="Stack ERC721/1155"
+                des=" Deploy a stacking contract for your NFTs. We will create a stacking contract for your NFTs. We will also create a website for your stacking contract where user can deposit NFTs and earn reward."
                 listItem={["Reactjs", "Tailwind CSS", "vercel deploy"]}
                 link="https://www.fiverr.com/"
               />
@@ -133,9 +128,8 @@ const Archive = () => {
               transition={{ delay: 0.6 }}
             >
               <ArchiveCard
-                title="Nextjs Dynamic Portfolio"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="Airdrop Services"
+                des=" We will create differents aidrop for your project depending on your requirements. This can allow project onwer to reward thier community with an On-Chain process."
                 listItem={["Reactjs", "Tailwind CSS", "vercel deploy"]}
                 link="https://www.fiverr.com/"
               />
@@ -146,9 +140,8 @@ const Archive = () => {
               transition={{ delay: 0.7 }}
             >
               <ArchiveCard
-                title="Bazaar - A Modern Shopping app"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="ERC4907 Rentable NFTs"
+                des=" NFT smart contract to rent NFTs instaed of selling them. "
                 listItem={["Reactjs", "Tailwind CSS", "vercel deploy"]}
                 link="https://www.fiverr.com/"
               />
@@ -159,9 +152,8 @@ const Archive = () => {
               transition={{ delay: 0.8 }}
             >
               <ArchiveCard
-                title="Blog101 Application"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="Loot Box NFTs"
+                des=" Pack multiple tokens into ERC1155 token and sell them as a loot box."
                 listItem={["Nextjs", "Tailwind CSS", "next-auth"]}
                 link="https://www.fiverr.com/"
               />
@@ -172,9 +164,8 @@ const Archive = () => {
               transition={{ delay: 0.9 }}
             >
               <ArchiveCard
-                title="Todo application"
-                des=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
-            laudantium velit qui sint non, voluptates quo a veritatis."
+                title="Membership And Community Stream"
+                des=" Membership is a contract that create time-bound NFT for usage like subscription, ticket, access, certifications. Community Stream is a contract that allow project onwer to distribute any token to a NFT holder community"
                 listItem={["Reactjs", "Tailwind CSS", "vercel deploy"]}
                 link="https://www.fiverr.com/"
               />
